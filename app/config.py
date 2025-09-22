fed43a14c01d10386c0494b64c1e9cae267e5136
rashmithahansamal <hansaxlord2001@gmail.com>
2025-09-22 12:31:05 +0530

    added new UI & working prototype

--- app/config.py
+++ app/config.py
@@ -2,8 +2,8 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 class Config:
-    OPEN_API_KEY = os.getenv("OPEN_API_KEY")
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
     AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
     AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
     AWS_BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
-    VECTOR_OB_PATH = 'vector_db'
+    VECTOR_DB_PATH = 'vector_db'
--- app/models/vector_store.py
+++ app/models/vector_store.py
@@ -10,7 +10,7 @@ class VectorStore:
             embedding_function=self.embeddings)
 
     def add_documents(self, documents):
-        self.vwctor_store.add_documents(documents)
+        self.vector_store.add_documents(documents)
 
     def similarity_search(self, query, k=4):
         return self.vector_store.similarity_search(query, k=k)
